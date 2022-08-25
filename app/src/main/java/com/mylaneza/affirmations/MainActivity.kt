60ce0157d542478e56a5f8ecc94d01e73b835c78
Ignacio Gonzalez <mylaneza@gmail.com>
2022-08-25 11:48:08 -0500

    Adding a RecyclerView to your app

--- app/src/main/java/com/mylaneza/affirmations/MainActivity.kt
+++ app/src/main/java/com/mylaneza/affirmations/MainActivity.kt
@@ -2,6 +2,10 @@ package com.mylaneza.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.mylaneza.affirmations.adapter.ItemAdapter
+import com.mylaneza.affirmations.data.Datasource
+
 //import android.widget.TextView
 //import com.mylaneza.affirmations.data.Datasource
 
@@ -13,5 +17,12 @@ class MainActivity : AppCompatActivity() {
         //3. Setting up the list of data
         /*val textView : TextView = findViewById(R.id.textview)
         textView.text = Datasource().loadAffirmations().size.toString()*/
+
+        //4. Adding a RecyclerView to your app
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this,myDataset)
+        //Optional for fixed sized data
+        recyclerView.setHasFixedSize(true)
     }
 }
---
+++ app/src/main/java/com/mylaneza/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,30 @@
+package com.mylaneza.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mylaneza.affirmations.R
+import com.mylaneza.affirmations.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item,parent,false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+
+    override fun getItemCount() = dataset.size
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view){
+        val textView : TextView = view.findViewById(R.id.item_title)
+    }
+}
