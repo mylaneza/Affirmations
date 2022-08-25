be52c3c56ce8d5c3b8361ccfc7edf4a19b731b69
Ignacio Gonzalez <mylaneza@gmail.com>
2022-08-25 11:16:50 -0500

    Setting up the list of data

--- app/src/main/java/com/mylaneza/affirmations/MainActivity.kt
+++ app/src/main/java/com/mylaneza/affirmations/MainActivity.kt
@@ -2,10 +2,16 @@ package com.mylaneza.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.widget.TextView
+//import com.mylaneza.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //3. Setting up the list of data
+        /*val textView : TextView = findViewById(R.id.textview)
+        textView.text = Datasource().loadAffirmations().size.toString()*/
     }
 }
---
+++ app/src/main/java/com/mylaneza/affirmations/model/Affirmation.kt
@@ -0,0 +1,4 @@
+package com.mylaneza.affirmations.model
+
+data class Affirmation(val stringResourceId : Int) {
+}
